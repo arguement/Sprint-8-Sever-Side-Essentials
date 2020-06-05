2c16a35c7cd0602634828309b3715d953e540f0e
Lanai Nevers <lanainevers@gmail.com>
2020-06-04 19:56:23 -0500

    Edit DB Name

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 CSRF_ENABLED = True
-SQLALCHEMY_DATABASE_URI = "mysql://root:@localhost/myncb"
+SQLALCHEMY_DATABASE_URI = "mysql://root:@localhost/testncb"
 SECRET_KEY = "THE a1l P0w3rFul N8 ComManDZ ALL"
